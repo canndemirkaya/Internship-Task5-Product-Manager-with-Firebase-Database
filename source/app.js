4be7d9c7d0459bf1f4a0c668dc8e0f8fb656a30d
Can <52953418+canndemirkaya@users.noreply.github.com>
2021-08-04 17:14:47 +0300

    app.js

--- source/app.js
+++ source/app.js
@@ -24,47 +24,7 @@
     //firebase database tanımlandı.
     const database = firebase.database();
     const rootRef = database.ref('products');
-    //tabloya eklem işlemi
-    rootRef.on('value', function (snapshot) {
-        snapshot.forEach(function (childSnapshot) {
-            var data = childSnapshot.val();
-            var productCode = data.productCode;
-            var productName = data.productName;
-            var categoryName = data.categoryName;
-            var brandName = data.brandName;
-            var price = data.price;
-            var tbodyRef = document.getElementById('myTable').getElementsByTagName('tbody')[0];
-            var newRow = tbodyRef.insertRow();
-            var newCell = newRow.insertCell();
-            var newCell2 = newRow.insertCell();
-            var newCell3 = newRow.insertCell();
-            var newCell4 = newRow.insertCell(); 
-            var newCell5 = newRow.insertCell();
-            var newText = document.createTextNode(productCode);
-            var newText2 = document.createTextNode(productName);
-            var newText3 = document.createTextNode(categoryName);
-            var newText4 = document.createTextNode(brandName);
-            var newText5 = document.createTextNode(price);
-
-            newCell.appendChild(newText);
-            newCell2.appendChild(newText2);
-            newCell3.appendChild(newText3);
-            newCell4.appendChild(newText4);
-            newCell5.appendChild(newText5);
-        });
-        //arama butonu komutları
-        const searchInput = document.getElementById("search");
-        const rows = document.querySelectorAll("tbody tr");
-        console.log(rows);
-        searchInput.addEventListener("keyup", function (event) {
-            const q = event.target.value.toLowerCase();
-            rows.forEach((row) => {
-                row.querySelector("td").textContent.toLowerCase().startsWith(q)
-                    ? (row.style.display = "table-row")
-                    : (row.style.display = "none");
-            });
-        });
-    });
+    
     //data güncelleme kısmı
     updateBtn.addEventListener('click', (e) =>{
         e.preventDefault();
@@ -91,6 +51,47 @@
         //     console.error(error);
         // });
     });
+    //tabloya ekleme işlemi
+        rootRef.on('value', function (snapshot) {
+            snapshot.forEach(function (childSnapshot) {
+                var data = childSnapshot.val();
+                var productCode = data.productCode;
+                var productName = data.productName;
+                var categoryName = data.categoryName;
+                var brandName = data.brandName;
+                var price = data.price;
+                var tbodyRef = document.getElementById('myTable').getElementsByTagName('tbody')[0];
+                var newRow = tbodyRef.insertRow();
+                var newCell = newRow.insertCell();
+                var newCell2 = newRow.insertCell();
+                var newCell3 = newRow.insertCell();
+                var newCell4 = newRow.insertCell(); 
+                var newCell5 = newRow.insertCell();
+                var newText = document.createTextNode(productCode);
+                var newText2 = document.createTextNode(productName);
+                var newText3 = document.createTextNode(categoryName);
+                var newText4 = document.createTextNode(brandName);
+                var newText5 = document.createTextNode(price);
+
+                newCell.appendChild(newText);
+                newCell2.appendChild(newText2);
+                newCell3.appendChild(newText3);
+                newCell4.appendChild(newText4);
+                newCell5.appendChild(newText5);
+            });
+            //arama butonu komutları
+            const searchInput = document.getElementById("search");
+            const rows = document.querySelectorAll("tbody tr");
+            console.log(rows);
+            searchInput.addEventListener("keyup", function (event) {
+                const q = event.target.value.toLowerCase();
+                rows.forEach((row) => {
+                    row.querySelector("td").textContent.toLowerCase().startsWith(q)
+                        ? (row.style.display = "table-row")
+                        : (row.style.display = "none");
+                });
+            });
+        });
     //data ekleme işlemi
     addBtn.addEventListener('click', (e) => {
         e.preventDefault();
